764c61e4f23ed8b621cc5cad659ecaad92a5af31
Franco Victorio <victorio.franco@gmail.com>
2013-11-17 21:48:39 -0300

    Initial commit

---
+++ graph.h
@@ -0,0 +1,25 @@
+#ifndef DOM_SET_GRAPH_H
+#define DOM_SET_GRAPH_H
+#include <map>
+#include <set>
+
+class Graph {
+public:
+    Graph () {}
+
+    void add_edge (int a, int b) { 
+        edges[a].insert(b); 
+        edges[b].insert(a);
+    }
+
+    const std::set<int>& neighbors (int a) {
+        return edges[a];
+    }
+
+    int size () const { return edges.size(); }
+    bool empty () const { return edges.empty(); }
+private:
+    std::map<int, std::set<int> > edges;
+};
+
+#endif
